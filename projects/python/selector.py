97e7fb9e106f8fc1b8d22edb0c8beb88b11beb2e
Eureka <teklemariameureka@gmail.com>
2024-02-05 12:59:54 +0300

    Add projects

---
+++ projects/python/selector.py
@@ -0,0 +1,29 @@
+# This is a program that selects random number form a given range just like the betting number games
+
+
+import random
+
+user = []
+
+for i in range(5):
+    num = int(input('Enter 5 numbers form the range of 1 to 80: '))
+    user.append(num)
+
+lst = []
+for i in range(10):
+    computer = random.randrange(1,80)
+    lst.append(computer)
+
+    print(f'number: {computer}')
+
+lst.sort()
+user.sort()
+
+count = 0
+if user ==lst:
+    print(f'Kudo you win you got all numbers right')
+else:
+    for i in user:
+        if i in lst:
+            count +=1
+    print(f'You got {count} guess right')
