c915a0f234ba656437ab5c30de0083f1fe87562c
Eureka <teklemariameureka@gmail.com>
2024-02-06 10:43:56 +0300

    Add  ListTupleDict/

---
+++ hackerRank/cpp/Hello.cpp
@@ -0,0 +1,11 @@
+//https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "ello, World!";
+    return 0;
+}
---
+++ hackerRank/cpp/inputOutput.cpp
@@ -0,0 +1,21 @@
+
+//https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    
+    int a,b,c;
+    cin >>a>>b>>c;
+    
+    int sum = a+b+c;
+    
+    cout <<sum;
+    
+    
+    
+    return 0;
+}
