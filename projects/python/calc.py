e7d0c36c2294cc2a60b3ab4794de4ce030bbd015
Eureka <teklemariameureka@gmail.com>
2024-02-16 01:37:42 +0300

    Add projects/python/calc.py

--- projects/python/calc.py
+++ projects/python/calc.py
@@ -2,24 +2,22 @@
 
 class Calculator:
 
-    def __init__(self,x,y):
+    def __init__(self,x):
         self.x = x
-        self.y = y
+
+    def __add__(self,number2):
+        return self.x + number2.x
+    
+    def __sub__(self,number2):
+        return self.x - number2.x
+    
+    def __mul__(self,number2):
+        return self.x * number2.x
+    
+    def __div__(self,number2):
+        return self.x / number2.x
 
 
-    def addition(self):
-        return self.x + self.y
-    
-    def subtraction(self):
-        return f"{self.x} - {self.y}"
-    
-    def mult(self):
-        return f"{self.x} * {self.y}"
-    
-
-    def division(self):
-        return f"{self.x} / {self.y}"
-    
 
 print('''
             |                               |
@@ -33,6 +31,7 @@ while True:
     print('2.Subtraction')
     print('3.Multiplication')
     print('4.Division')
+    print('5.Quit')
 
 
     try:
@@ -45,9 +44,62 @@ while True:
             try:
                 num1 = int(input("Enter the first number: "))
                 num2 = int(input("Enter the other number:  "))
+
+                number1 = Calculator(num1)
+                number2 = Calculator(num2)
             except ValueError as e:
                 print(e)
                 print("Please enter the input in a correct format")
             else:
-                result = Calculator(num1,num2)
-                print(result.addition())
+                print(number1 + number2)
+        
+        elif user == 2:
+            try:
+                num1 = int(input("Enter the first number: "))
+                num2 = int(input("Enter the other number:  "))
+
+                number1 = Calculator(num1)
+                number2 = Calculator(num2)
+            except ValueError as e:
+                print(e)
+                print("Please enter the input in a correct format")
+            else:
+                print(number1 - number2)
+           
+        elif user == 3:
+            try:
+                num1 = int(input("Enter the first number: "))
+                num2 = int(input("Enter the other number:  "))
+
+                number1 = Calculator(num1)
+                number2 = Calculator(num2)
+            except ValueError as e:
+                print(e)
+                print("Please enter the input in a correct format")
+            else:
+                print(number1 * number2)
+
+        elif user ==4:
+            try: 
+                num1 = int(input("ENter the first number:  "))
+                num2 = int(input("Enter the other number:  "))
+                
+                number1 = Calculator(num1)
+                number2 = Calculator(num2)
+
+            except ZeroDivisionError as e:
+                print(e)
+                print('We cant divide a number by zero so please try to finx your input')
+            except ValueError as e:
+                print(e)
+                print("Pleae enter your input correctly")
+
+        elif user == 5:
+            try:
+                break
+            except ValueError as e:
+                print(e)
+                print("Pleae enter your value correctly")
+        else:
+
+            print("Please enter the correct choice")
