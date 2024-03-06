431a14ddf81620808aef4f0c9a8fad5787217d82
Eureka <teklemariameureka@gmail.com>
2024-03-06 10:48:48 +0300

    Add exceptions.py

--- hackerRank/python/exceptions.py
+++ hackerRank/python/exceptions.py
@@ -1,18 +1,12 @@
-# https://www.hackerrank.com/challenges/exceptions/problem?isFullScreen=true
-
 
 
 # Error Code: integer division or modulo by zero
 # Error Code: invalid literal for int() with base 10: '$'
 
 
-try:
-    a = int(input())
-    b = int(input())
-    result = a / b
-except ZeroDivisionError as e:
-    print(e)
-except ValueError as e:
-    print(e)
-else:
-    print(result)
+for i in range(int(input())):
+    try:
+        a, b = map(int, input().split())
+        print(int(a//b))
+    except Exception as e:
+        print("Error Code:",e)
