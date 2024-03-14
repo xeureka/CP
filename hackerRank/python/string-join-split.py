e5114eb86e5dc32c1ebeb0e6cd0527389cdce6d2
Eureka <teklemariameureka@gmail.com>
2024-03-15 01:19:25 +0300

    Add string-join-split.py

---
+++ hackerRank/python/string-join-split.py
@@ -0,0 +1,13 @@
+#https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+
+def split_and_join(line):
+    # write your code here
+    line = line.split(" ")
+    line = '-'.join(line)
+    return line
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
