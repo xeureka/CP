f49fd01cc8577223e8252d76fa9dcbb2f8348d94
Eureka <teklemariameureka@gmail.com>
2024-03-15 23:10:25 +0300

    Add set.add.py

---
+++ hackerRank/python/set.add.py
@@ -0,0 +1,15 @@
+
+
+# https://www.hackerrank.com/challenges/py-set-add/problem?isFullScreen=true
+
+
+
+n = int(input())
+set1 = {}
+set1= set(set1)
+for i in range(1,n+1):
+    usr = input()
+    set1.add(usr)
+    
+    
+print(len(set1))
