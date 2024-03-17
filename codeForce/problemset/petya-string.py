959ff3723a30953490d50553a61d2f1f4139be59
Eureka <teklemariameureka@gmail.com>
2024-03-17 03:23:06 +0300

    Add problemset

---
+++ codeForce/problemset/petya-string.py
@@ -0,0 +1,14 @@
+
+
+#https://codeforces.com/problemset/status?my=on
+
+
+word1 = input().lower()
+word2 = input().lower()
+
+if word1 > word2:
+    print(1)
+elif word1 < word2:
+    print(-1)
+else:
+    print(0)
