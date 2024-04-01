85c92b0e98399e851fa1d6656dc2c30d593ab447
Eureka <teklemariameureka@gmail.com>
2024-04-01 22:06:02 +0300

    Add bit.cpp

---
+++ codeForce/problemset/bit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a;
+    int X =0;
+    string x;
+
+    cin >>a;
+    cin >>
+
+    for(int i ; i<=a;i++){
+
+    }
+
+
+
+    return 0;
+}
