5cc2a4027bada766f73f4afc3dde31a8b152862b
Eureka <teklemariameureka@gmail.com>
2024-04-03 00:10:29 +0300

    new commit

---
+++ codeForce/problemset/ Soldier and Bananas.py
@@ -0,0 +1,17 @@
+# https://codeforces.com/problemset/problem/546/A
+
+
+k,n,w = map(int,input().split())
+
+
+total = 0
+
+for i in range(1,w+1):
+    total += i * k
+
+borrow = total - n
+
+if borrow <=0:
+    print(0)
+else:
+    print(borrow)
