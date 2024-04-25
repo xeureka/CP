3676087d9c8f01d8742f37db9bf6ff9abec975e4
Eureka <teklemariameureka@gmail.com>
2024-04-25 09:37:23 +0300

    .

--- Leetcode/FizzBuzz.py
+++ Leetcode/FizzBuzz.py
@@ -1,8 +1,8 @@
 
-# https://leetcode.com/problems/fizz-buzz/
+# https://leetcode.com/problems/fizz-buzz/sna
     
 class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
+    def fizzBuzz(self, n: int) -> list[str]:
         lst = []
         for i in range(1,n+1):
             if i % 3 ==0 and i % 5 ==0:
