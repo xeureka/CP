4ea750685346bc77e15faf1538ec24b5261be446
Eureka <teklemariameureka@gmail.com>
2024-05-08 23:22:46 +0300

    Fainal solution

--- codeForce/problemset/Elephant.py
+++ codeForce/problemset/Elephant.py
@@ -1,10 +1,9 @@
 # https://codeforces.com/problemset/problem/617/A
 
 
+def min_step(x):
+    return (x//5) + (1 if x %5 != 0 else 0)
 
 n = int(input())
 
-if n % 5 ==0:
-    print(n /5)
-else:
-    print((n + 4)/5)
+print(min_step(n))
