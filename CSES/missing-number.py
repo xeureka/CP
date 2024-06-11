545fd61a79d1fdb600840828efa62a95a77a12b7
Eureka <teklemariameureka@gmail.com>
2024-06-11 21:53:52 +0300

    still wong answer

--- CSES/missing-number.py
+++ CSES/missing-number.py
@@ -1,11 +1,16 @@
 # https://cses.fi/problemset/task/1083
 
+def missingNumber(nums):
+    n = len(nums)
+
+    s = set(nums)
+
+    for num in range(n+1):
+        if num not in s:
+            return num
+        
+
 n = int(input())
+nums = list(map(int,input().split()))
 
-lst = list(map(int,input().split()))
-
-lst.sort()
-
-for i in range(n):
-    for i in lst:
-        if lst[i] 
+missingNumber(nums)
