d51dd860122e27531e3d9b256f3b19bc95a42cea
Eureka <teklemariameureka@gmail.com>
2024-06-24 03:58:23 +0300

    capitalize locally solved but not i the hacerrank website

--- hackerRank/python/capitalize.py
+++ hackerRank/python/capitalize.py
@@ -1,16 +1,26 @@
 
-name = input('Enter your name:  ')
-
-x = name.rstrip(' ')
-
-print(x)
 
 
+# https://www.hackerrank.com/challenges/capitalize/problem?isFullScreen=true
 
 
+word = input()
 
+def solve(s):
+    num = ['1','2','3','4','5','6','7','8','9','0']
+    
+    name = word.split(' ')
+    ans = ''
+    
+    for item in name:
+        if item[0] in num:
+            ans += item
+            ans += ' '
+        else:
+            ans += item.title()
+            ans += ' '   
+            
+             
+    return ans
 
-
-
-
-
+print(solve(word))
