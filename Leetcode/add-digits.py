2dafcc4dba46daf404d138a7253516db7d83b9f5
Eureka <teklemariameureka@gmail.com>
2024-06-30 02:24:24 +0300

    Insert position and add digits

---
+++ Leetcode/add-digits.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/add-digits/
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+
+        while num >=10:
+            num = sum(int(i) for i in str(num))
+        return num
+        
---
+++ Leetcode/insert-position.py
@@ -0,0 +1,13 @@
+
+# https://leetcode.com/problems/search-insert-position/
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+
+            return nums.index(target)
