f06dd0d831845293c728db3b72d90d83c80841b4
Eureka <teklemariameureka@gmail.com>
2024-07-03 22:20:30 +0300

    DAY 2

---
+++ Leetcode/JS-30/counter.js
@@ -0,0 +1,2 @@
+// https://leetcode.com/problems/counter/description/?envType=study-plan-v2&envId=30-days-of-javascripthttps://leetcode.com/problems/counter/description/?envType=study-plan-v2&envId=30-days-of-javascript
+
