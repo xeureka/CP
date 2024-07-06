07f22f4cbe35d78044bb3c5efe9865064075fbbb
Eureka <teklemariameureka@gmail.com>
2024-07-07 00:06:13 +0300

    Beautiful year not solved

--- codeForce/problemset/BEFORE/beautiful-year.py
+++ codeForce/problemset/BEFORE/beautiful-year.py
@@ -1,8 +1,24 @@
 # https://codeforces.com/problemset/problem/271/A
+from collections import Counter
 
-year = input()
+year  = int(input())
 
-while True:
-    year +=1
-    
-    new = str(year)
+def dis_year(year):
+    new = Counter(str(year))
+
+    value = True
+
+    for item in new:
+        if new[item] ==1:
+            value = True
+        else:
+            break
+
+        return value
+        
+
+
+for i in range(year+1,9000):
+    if dis_year(year):
+        print(i)
+        break
