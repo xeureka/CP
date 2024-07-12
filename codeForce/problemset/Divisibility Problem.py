e346bb57dd1e6d075374efc6b0ed6c54f56122b6
xeureka <teklemariameureka@gmail.com>
2024-07-13 02:01:25 +0300

    TLE resolved

--- codeForce/problemset/Divisibility Problem.py
+++ codeForce/problemset/Divisibility Problem.py
@@ -1,19 +1,16 @@
 # https://codeforces.com/problemset/problem/1328/A
 
 
-def func(a,b):
-        cout = 0
-
-        while True:
-            if a % b ==0:
-                return cout
-            else:
-                a +=1
-                cout +=1
-
 for i in range(int(input())):
     a,b = map(int,input().split())
-    print(func(a,b))
+    
+    v = a/b
+
+    if v - int(v) ==0:
+        print(0)
+    else:
+        n = b*(int(v) +1)
+        print(n -b)
 
     
 
