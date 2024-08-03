23f3a04af3093fb6a26b8b4fa7e321928c2f149c
xeureka <teklemariameureka@gmail.com>
2024-08-03 06:33:42 +0300

    10 day of js

--- kattis/AAAAtest.py
+++ kattis/AAAAtest.py
@@ -1,1 +1,15 @@
 # this is test site
+# /https://open.kattis.com/problems/skammstofun?editresubmit=13973998
+
+n = int(input())
+
+word = input()
+
+
+new = ''
+
+for i in word:
+    if i.isupper():
+        new += i
+
+print(new)
