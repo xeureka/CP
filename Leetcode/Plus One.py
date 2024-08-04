fd7ce3520bbb73e37006d9354303a79f818a8ced
xeureka <teklemariameureka@gmail.com>
2024-08-04 22:04:50 +0300

    Plus one

---
+++ Leetcode/Plus One.py
@@ -0,0 +1,21 @@
+
+# https://leetcode.com/problems/plus-one/
+
+class Solution(object):
+    def plusOne(self, digits):
+        num = ''
+
+        for i in digits:
+            i = str(i)
+            num += i
+
+                
+        lst = []
+
+        for i in str(int(num) + 1):
+            i = int(i)
+            lst.append(i)
+        
+        return lst
+        
+        
---
+++ Leetcode/test.py
@@ -0,0 +1,21 @@
+
+
+digits = [1,2,3]
+
+num = ''
+
+for i in digits:
+    i = str(i)
+    num += i
+
+
+ans = int(num)+1
+ans = str(ans)
+
+lst = []
+
+for i in ans:
+    i = int(i)
+    lst.append(i)
+
+print(lst)
