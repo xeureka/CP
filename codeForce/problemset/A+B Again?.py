5b64936ed72feebf6b2a9c7fb13a960025837e5e
xeureka <teklemariameureka@gmail.com>
2024-08-06 09:33:41 +0300

    A+b 2

---
+++ codeForce/problemset/A+B Again?.py
@@ -0,0 +1,12 @@
+
+# https://codeforces.com/problemset/problem/1999/A
+
+for i in range(int(input())):
+    n = input()
+
+    sum = 0
+
+    for i in n:
+        sum += int(i)
+    print(sum)
+
--- codeForce/problemset/AAA.py
+++ codeForce/problemset/AAA.py
@@ -1,2 +1,57 @@
+# for i in range(int(input())):
+#     n = input()
+
+#     sum = 0
+
+#     for i in n:
+#         sum += int(i)
+#     print(sum)
+
+# problem 2 starts here
+
+for i in range(int(input())):
+    lst = list(map(int,input().split()))
+    su = lst[:2]
+    sl = lst[2:]
+
+    comb = list(zip(su,sl))
+
+    rev = su[::-1]
+
+    comb2 = list(zip(rev,sl))
+
+    count = 0
+
+    answer = 0
+
+    for i in comb:
+        if i[0] > i[1]:
+            count += 1
+
+    if count == 2:
+        answer += 1
+
+    count2 = 0
+
+    for i in comb2:
+        if i[0] > i[1]:
+            count2 += 1
+
+    if count2 ==2:
+        answer += 1
+
+    print(answer*2)
+
+     
+
+    
 
 
+        
+
+
+    
+
+    
+        
+    
