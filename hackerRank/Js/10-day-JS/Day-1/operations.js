e570478240806d0dfca42a30cf9f6b60e7500990
xeureka <teklemariameureka@gmail.com>
2024-08-11 01:14:54 +0300

    Day 1 of js challenge

---
+++ hackerRank/Js/10-day-JS/Day-1/operations.js
@@ -0,0 +1,14 @@
+function getArea(length, width) {
+    let area = length * width;
+    // Write your code here
+    
+    return area;
+}
+
+
+function getPerimeter(length, width) {
+    let perimeter = 2*length + 2*width;
+    // Write your code here
+    
+    return perimeter;
+}
