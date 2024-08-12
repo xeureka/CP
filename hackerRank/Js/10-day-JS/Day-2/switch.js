5132de7ecd7c705ebff1aa985e0253af7ba1f70d
xeureka <teklemariameureka@gmail.com>
2024-08-12 09:43:38 +0300

    switch

---
+++ hackerRank/Js/10-day-JS/Day-2/switch.js
@@ -0,0 +1,16 @@
+switch (s.charAt(0)){
+    case ('a' || 'e' || 'i' || 'o' ||'u'):
+        letter = 'A';
+        break;
+    case ('b'||'c'||'d'||'f'||'g'):
+        letter = 'B';
+        break;
+    case ('h'||'j'||'k'||'l'||'m'):
+        letter = 'C';
+        break;
+    default:
+        letter = 'D';
+        break
+
+}
+  
