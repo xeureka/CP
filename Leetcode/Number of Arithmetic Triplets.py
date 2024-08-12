53e681704833e5c08a27920798cd1a74c71b82f3
xeureka <teklemariameureka@gmail.com>
2024-08-12 08:34:48 +0300

    Number of Arithmetic Triplets

---
+++ Leetcode/Number of Arithmetic Triplets.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/number-of-arithmetic-triplets/description/
+
+
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                for k in range(len(nums)):
+                    if (i < j < k) and (nums[j] - nums[i] == diff) and (nums[k] - nums[j] == diff):
+                        count += 1
+        return count
