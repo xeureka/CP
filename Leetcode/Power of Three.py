254e77852ac2542da3918134ed45e49e4ad3d0ea
xeureka <teklemariameureka@gmail.com>
2024-08-13 23:47:57 +0300

    Power of Three

---
+++ Leetcode/Power of Three.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/power-of-three/description/
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        else:
+            return(math.ceil(math.log10(n)/math.log10(3))==math.floor(math.log10(n)/math.log10(3)))
+
+        
