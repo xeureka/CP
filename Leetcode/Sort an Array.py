d574a27937f2c86cbea471fb885122e36b1cecf0
xeureka <teklemariameureka@gmail.com>
2024-08-14 08:52:37 +0300

    Sort an Array

---
+++ Leetcode/Sort an Array.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/sort-an-array/description/
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        return nums
+        
+        
