8bc8a29cfd4a21f616a01fd005e152c333615ca4
xeureka <teklemariameureka@gmail.com>
2024-08-17 22:16:12 +0300

    odd index list

---
+++ pynative/Loop/15.py
@@ -0,0 +1,8 @@
+# Exercise 15: Use a loop to display elements from a given list present at odd index positions
+
+
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+for i in range(len(my_list)):
+    if i % 2 != 0:
+        print(my_list[i],end=' ')
