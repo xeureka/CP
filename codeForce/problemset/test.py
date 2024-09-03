d34d33681125f8c836c4987edb6fe061c1b900d2
xeureka <teklemariameureka@gmail.com>
2024-09-04 01:21:32 +0300

    test

--- codeForce/problemset/test.py
+++ codeForce/problemset/test.py
@@ -1,25 +1,15 @@
-
-def find_missing_integer(integers):
-    
-    integer_set = set(integers)
-    
-    for x in range(2, 21):  
-        n = 10 ** x
-        
-        
-        if n not in integer_set:
-            if int(str(n)[:-x]) in integer_set:
-                return n
-    
-    return None
-
-
-
-
-
-
 for i in range(int(input())):
-    word = int(input())
-    print(find_missing_integer(word))
+    a,b = map(int,input().split())
 
-    
+    ans = []
+
+    for c in range(a+1,b):
+        val = (c - a) + (c - b)
+        ans.append((val,c))
+
+    ans.sort()
+
+    print(ans)
+        
+
+
