2a5fab5c2776db1189379dd59045b0da02c46f29
xeureka <teklemariameureka@gmail.com>
2024-09-05 01:42:58 +0300

    1

---
+++ pynative/Functions/1.py
@@ -0,0 +1,7 @@
+# Write a program to create a function that takes two arguments, name and age, and print their value.
+
+
+def display(name,age):
+    return f'Name = {name}\nAge = {age}'
+
+print(display('Eureka',20))
