95ea563c456ed6133985055a4639b7435d6c8e23
xeureka <teklemariameureka@gmail.com>
2024-09-29 09:07:41 +0300

    Add leetcode test

--- Leetcode/test.py
+++ Leetcode/test.py
@@ -1,11 +1,10 @@
-word = "abcd"
-ch = "z"
+nums = [-1,0,1,2,-1,-4]
+ans = []
 
-num = word.index(ch)
-
-w = word[:num+1]
-other = word[num+1:]
-
-ans = w[::-1] + other
+for i in range(len(nums)):
+    for j in range(i+1,len(nums)):
+        for k in range(j+1,len(nums)):
+            if (i != j and i != k and j != k) and (nums[i] + nums[j] + nums[k] == 0):
+                ans.append([nums[i],nums[j],nums[k]])
 
 print(ans)
