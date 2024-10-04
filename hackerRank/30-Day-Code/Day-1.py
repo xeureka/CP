3a1dfd94835460ff89921640a9654b03a7bf4cf0
xeureka <teklemariameureka@gmail.com>
2024-10-04 08:04:09 +0300

    day 1 of 30 day challenge

---
+++ hackerRank/30-Day-Code/Day-1.py
@@ -0,0 +1,22 @@
+
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a_int = int(input())
+
+# Read and save an integer, double, and String to your variables.
+b_double = float(input())
+
+# Print the sum of both integer variables on a new line.
+c_string = input()
+
+
+
+# Print the sum of the double variables on a new line.
+print(i+a_int)
+print(d+b_double)
+print(s + c_string)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
---
+++ hackerRank/30-Day-Code/README.md
@@ -0,0 +1,3 @@
+A repo for Hacker ranks 30 day coding challenge
+
+chellenge link = https://www.hackerrank.com/domains/tutorials/30-days-of-code
