4d36374b681ae936c7ad25902e6ea24d18d0dab4
xeureka <teklemariameureka@gmail.com>
2024-10-05 03:03:36 +0300

    day 2 of hackerank 30 day challenge

--- Leetcode/test.py
+++ Leetcode/test.py
@@ -1,13 +1,4 @@
 words = ["bella","label","roller"]
 
-words = tuple(words)
-
-first,*rest = words
-ans = []
-
-for i in first:
-    for j in rest:
-        if i in j:
-            ans.append(i)
-
-print(ans)
+check = words[0]
+    
