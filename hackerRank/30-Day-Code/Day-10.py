358b810dfd11c7aadf361e8db907832ca982f6b5
xeureka <teklemariameureka@gmail.com>
2024-10-23 16:08:17 +0300

    day 10 of hackerrank

--- hackerRank/30-Day-Code/Day-10.py
+++ hackerRank/30-Day-Code/Day-10.py
@@ -1,11 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
 
 
-n = int(input())
 
-binary = bin(n)[2:]
-
-count = 0
-
-for i in binary:
-    if i == '1'
-    pass
+if __name__ == '__main__':
+    n = int(input().strip())
+    num = bin(n)
+    
+    maxim = 0
+    current = 0
+    
+    for i in num:
+        if i == '1':
+            current += 1
+        else:
+            maxim = max(maxim,current)
+            current = 0
+    
+    print(max(maxim,current))
+    
+    
+    
+    
