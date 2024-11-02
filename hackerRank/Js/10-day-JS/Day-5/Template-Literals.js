21392d5104e5351ddb740ade0d7beaaa9512c296
xeureka <teklemariameureka@gmail.com>
2024-11-03 01:53:45 +0300

    template literals

---
+++ hackerRank/Js/10-day-JS/Day-5/Template-Literals.js
@@ -0,0 +1,71 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function calcAreaPerim(a,p){
+    const result = [];
+
+    let disc = Math.sqrt((p*p) - (16*a))
+
+    let s1 = p + disc;
+    let s2 = p - disc;
+
+    result.push(s1 /4.0)
+    result.push(s2/4.0)
+
+    result.sort(function(a,b){
+        return a - b
+    })
+
+    return result
+}
+function sides(literals, ...expressions) {
+    let area = expressions[0];
+    let perim = expressions[1];
+
+    return calcAreaPerim(area,perim)
+    
+    
+}
+
+
+function main() {
+    let s1 = +(readLine());
+    let s2 = +(readLine());
+    
+    [s1, s2] = [s1, s2].sort();
+    
+    const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
+    
+    console.log((s1 === x) ? s1 : -1);
+    console.log((s2 === y) ? s2 : -1);
+}
