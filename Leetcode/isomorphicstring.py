28e62b671fd5f45f45cb73d04d16d3fb7452dc06
xeureka <teklemariameureka@gmail.com>
2024-12-06 15:39:48 +0300

    isomorphic string

---
+++ Leetcode/isomorphicstring.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/isomorphic-strings/description/
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+
+
+            if len(s) != len(t):
+                return False
+            sStack,tStack = [],[]
+
+            for i in range(len(s)):
+                sStack.append(s.index(s[i]))
+                tStack.append(t.index(t[i]))
+            
+            return sStack == tStack
+        
--- Leetcode/test.py
+++ Leetcode/test.py
@@ -1,32 +1,15 @@
-class Solution:
-  def threeSum(self, nums: list[int]) -> list[list[int]]:
-    if len(nums) < 3:
-      return []
+s = 'egg'
+t = 'add'
 
-    ans = []
+def solution(s,t):
+    if len(s) != len(t):
+        return False
+    sStack,tStack = [],[]
 
-    nums.sort()
+    for i in range(len(s)):
+        sStack.append(s.index(s[i]))
+        tStack.append(t.index(t[i]))
+    
+    return sStack == tStack
 
-    for i in range(len(nums) - 2):
-      if i > 0 and nums[i] == nums[i - 1]:
-        continue
-      # Choose nums[i] as the first number in the triplet, then search the
-      # remaining numbers in [i + 1, n - 1].
-      l = i + 1
-      r = len(nums) - 1
-      while l < r:
-        summ = nums[i] + nums[l] + nums[r]
-        if summ == 0:
-          ans.append((nums[i], nums[l], nums[r]))
-          l += 1
-          r -= 1
-          while nums[l] == nums[l - 1] and l < r:
-            l += 1
-          while nums[r] == nums[r + 1] and l < r:
-            r -= 1
-        elif summ < 0:
-          l += 1
-        else:
-          r -= 1
-
-    return ans
+print(solution(s,t))
